e38b3fe62a054db85bd866229b10b853ca8090f1
Ryotankobu <ucp4fya7@yahoo.co.jp>
2024-12-11 15:08:23 +1000

    serviceworker examining

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,6 +5,7 @@ const urlsToCache = ["/", "/index.html", "/App.js", "/App.css"];
 
 // Install Service Worker
 self.addEventListener("install", (event) => {
+  console.log("Service Worker installing...");
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log("Opened cache");
@@ -15,6 +16,7 @@ self.addEventListener("install", (event) => {
 
 // Fetch from Cache
 self.addEventListener("fetch", (event) => {
+  console.log("Fetching:", event.request.url);
   event.respondWith(
     caches.match(event.request).then((response) => {
       // Return cached resource or fetch from the network
@@ -25,6 +27,7 @@ self.addEventListener("fetch", (event) => {
 
 // Activate Service Worker
 self.addEventListener("activate", (event) => {
+  console.log("Service Worker activating...");
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) =>
